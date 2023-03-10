2046f9cdbddb1d57da08cc6b69edf171e05750f5
Jason Hendee <jason@triumph.tech>
2023-03-10 10:15:22 -0700

    - (Group) Added boilerplate C# and JS Obsidian bits to support new Group Scheduler block.

---
+++ Rock.Blocks/Group/Scheduling/GroupScheduler.cs
@@ -0,0 +1,117 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.ComponentModel;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Field.Types;
+using Rock.ViewModels.Blocks.Group.Scheduling.GroupScheduler;
+
+namespace Rock.Blocks.Group.Scheduling
+{
+    /// <summary>
+    /// Allows group schedules for groups and locations to be managed by a scheduler.
+    /// </summary>
+
+    [DisplayName( "Group Scheduler" )]
+    [Category( "Group Scheduling" )]
+    [Description( "Allows group schedules for groups and locations to be managed by a scheduler." )]
+    [IconCssClass( "fa fa-calendar-alt" )]
+
+    #region Block Attributes
+
+    [BooleanField( "Enable Alternate Group Individual Selection",
+        Key = AttributeKey.EnableAlternateGroupIndividualSelection,
+        Description = "Determines if individuals may be selected from alternate groups.",
+        ControlType = BooleanFieldType.BooleanControlType.Checkbox,
+        DefaultBooleanValue = false,
+        Order = 0,
+        IsRequired = false )]
+
+    [BooleanField( "Enable Parent Group Individual Selection",
+        Key = AttributeKey.EnableParentGroupIndividualSelection,
+        Description = "Determines if individuals may be selected from parent groups.",
+        ControlType = BooleanFieldType.BooleanControlType.Checkbox,
+        DefaultBooleanValue = false,
+        Order = 1,
+        IsRequired = false )]
+
+    [BooleanField( "Enable Data View Individual Selection",
+        Key = AttributeKey.EnableDataViewIndividualSelection,
+        Description = "Determines if individuals may be selected from data views.",
+        ControlType = BooleanFieldType.BooleanControlType.Checkbox,
+        DefaultBooleanValue = false,
+        Order = 2,
+        IsRequired = false )]
+
+    #endregion
+
+    [Rock.SystemGuid.EntityTypeGuid( "7ADCE833-A785-4A54-9805-7335809C5367" )]
+    [Rock.SystemGuid.BlockTypeGuid( "511D8E2E-4AF3-48D8-88EF-2AB311CD47E0" )]
+    public class GroupScheduler : RockObsidianBlockType
+    {
+        #region Keys
+
+        private static class AttributeKey
+        {
+            public const string EnableAlternateGroupIndividualSelection = "EnableAlternateGroupIndividualSelection";
+            public const string EnableParentGroupIndividualSelection = "EnableParentGroupIndividualSelection";
+            public const string EnableDataViewIndividualSelection = "EnableDataViewIndividualSelection";
+        }
+
+        #endregion
+
+        #region Properties
+
+        public override string BlockFileUrl => $"{base.BlockFileUrl}.obs";
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                var box = new GroupSchedulerInitializationBox();
+
+                SetBoxInitialState( box, rockContext );
+
+                return box;
+            }
+        }
+
+        /// <summary>
+        /// Sets the initial state of the box.
+        /// </summary>
+        /// <param name="box">The box.</param>
+        /// <param name="rockContext">The rock context.</param>
+        private void SetBoxInitialState( GroupSchedulerInitializationBox box, RockContext rockContext )
+        {
+
+        }
+
+        #endregion
+
+        #region Block Actions
+
+
+
+        #endregion
+    }
+}
---
+++ Rock/Plugin/HotFixes/199_AddObsidianGroupScheduler.cs
@@ -0,0 +1,50 @@
+﻿namespace Rock.Plugin.HotFixes
+{
+    /// <summary>
+    /// Plug-in migration
+    /// </summary>
+    /// <seealso cref="Rock.Plugin.Migration" />
+    [MigrationNumber( 199, "1.14.1" )]
+    public class AddObsidianGroupScheduler : Migration
+    {
+        /// <summary>
+        /// Operations to be performed during the upgrade process.
+        /// </summary>
+        public override void Up()
+        {
+            AddObsidianGroupScheduler_AddPublicBlockTypes();
+        }
+
+        /// <summary>
+        /// Operations to be performed during the downgrade process.
+        /// </summary>
+        public override void Down()
+        {
+            ShortTermServingProjects_DeletePublicBlockTypes();
+        }
+
+        /// <summary>
+        /// JPH: Add public block types needed for the Obsidian Group Scheduler.
+        /// </summary>
+        private void AddObsidianGroupScheduler_AddPublicBlockTypes()
+        {
+            RockMigrationHelper.UpdateEntityType( "Rock.Blocks.Group.Scheduling.GroupScheduler", "Group Scheduler", "Rock.Blocks.Group.Scheduling.GroupScheduler, Rock.Blocks, Version=1.15.0.13, Culture=neutral, PublicKeyToken=null", false, false, "7ADCE833-A785-4A54-9805-7335809C5367" );
+            RockMigrationHelper.UpdateMobileBlockType( "Group Scheduler", "Allows group schedules for groups and locations to be managed by a scheduler.", "Rock.Blocks.Group.Scheduling.GroupScheduler", "Group Scheduling", "511D8E2E-4AF3-48D8-88EF-2AB311CD47E0" );
+            RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "511D8E2E-4AF3-48D8-88EF-2AB311CD47E0", "1EDAFDED-DFE6-4334-B019-6EECBA89E05A", "Enable Alternate Group Individual Selection", "EnableAlternateGroupIndividualSelection", "Enable Alternate Group Individual Selection", "Determines if individuals may be selected from alternate groups.", 0, "False", "BE4DAA0D-95AB-40A8-826B-9391691C068D" );
+            RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "511D8E2E-4AF3-48D8-88EF-2AB311CD47E0", "1EDAFDED-DFE6-4334-B019-6EECBA89E05A", "Enable Parent Group Individual Selection", "EnableParentGroupIndividualSelection", "Enable Parent Group Individual Selection", "Determines if individuals may be selected from parent groups.", 1, "False", "131BEF8F-55CE-4D6B-8EB2-B18449975B2C" );
+            RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "511D8E2E-4AF3-48D8-88EF-2AB311CD47E0", "1EDAFDED-DFE6-4334-B019-6EECBA89E05A", "Enable Data View Individual Selection", "EnableDataViewIndividualSelection", "Enable Data View Individual Selection", "Determines if individuals may be selected from data views.", 2, "False", "6CA4FDDC-9B2F-406F-A7A6-583E25B0B942" );
+        }
+
+        /// <summary>
+        /// /// JPH: Delete public block types added for the Obsidian Group Scheduler.
+        /// </summary>
+        private void ShortTermServingProjects_DeletePublicBlockTypes()
+        {
+            RockMigrationHelper.DeleteBlockAttribute( "6CA4FDDC-9B2F-406F-A7A6-583E25B0B942" );
+            RockMigrationHelper.DeleteBlockAttribute( "131BEF8F-55CE-4D6B-8EB2-B18449975B2C" );
+            RockMigrationHelper.DeleteBlockAttribute( "BE4DAA0D-95AB-40A8-826B-9391691C068D" );
+            RockMigrationHelper.DeleteBlockType( "511D8E2E-4AF3-48D8-88EF-2AB311CD47E0" );
+            RockMigrationHelper.DeleteEntityType( "7ADCE833-A785-4A54-9805-7335809C5367" );
+        }
+    }
+}
